154af8c70b78b7d2c0295023c1bbc43875dead88
Stefan Eng <stefaneng13@gmail.com>
2014-10-01 19:27:24 -0700

    List of factorial numbers

--- src/Main.hs
+++ src/Main.hs
@@ -3,5 +3,9 @@ module Main where
 numberOfTrailingZeros :: Int -> Int
 numberOfTrailingZeros = length . fst . break (/= '0') . reverse . show
 
+-- | List of factorial numbers
+factorial :: [Int]
+factorial = scanl (*) 1 [2..]
+
 main :: IO ()
 main = putStrLn "Hello world"
